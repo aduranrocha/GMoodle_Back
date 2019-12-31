01da6f8c975a3f53088a23d854e3ca0c684baecf
Ale Durán <aduranrocha@gmail.com>
2019-12-31 14:47:47 -0600

    Revert "Project started"
    This reverts commit 27b683a11b8676d32f41c4423d1a05b66c4c324b.

--- src/main/java/com/springapp/models/dao/ClienteDAO.java
+++
@@ -1,9 +0,0 @@
-package com.springapp.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.springapp.models.entity.Cliente;
-
-public interface ClienteDAO extends CrudRepository<Cliente, Long> {
-
-}
