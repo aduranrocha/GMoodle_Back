a5d1a71fd73ec2fcc23a4525db24fe424ceff3d7
Ale Durán <aduranrocha@gmail.com>
2019-12-31 16:07:20 -0600

    Project base

---
+++ src/main/java/com/gmoodle/GmoodleApplication.java
@@ -0,0 +1,13 @@
+package com.gmoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmoodleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmoodle/GmoodleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmoodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmoodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
