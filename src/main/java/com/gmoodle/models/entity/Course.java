fa9221d7cd898320eca5d9c9cd4ee83c7b26c9f9
Ale Durán <aduranrocha@gmail.com>
2020-01-02 17:54:56 -0600

    Creation of the entity class 'Course'

--- src/main/java/com/gmoodle/models/entity/Course.java
+++ src/main/java/com/gmoodle/models/entity/Course.java
@@ -1,5 +1,35 @@
 package com.gmoodle.models.entity;
 
-public class Course {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Course implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long idCourse;
+	
+	@NotEmpty(message="can not be empty")
+	@Size(min=4, max=44, message="the size must be between 4 and 44")
+	@Column(nullable=false,unique=true)
+	private String nameCourse;
+	
+	@NotEmpty(message ="can not be empty")
+	@Size(min=10, max=150, message="the size must be between 10 and 150")
+	@Column(nullable=false)
+	private String summaryCourse;
+	
+	@NotNull(message ="can not be empty")
+	@Column(name="createAt")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
 }
