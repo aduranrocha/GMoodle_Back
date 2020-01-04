b0dbe7f52f7ddaac39af6697674da538727fd15b
Ale Durán <aduranrocha@gmail.com>
2020-01-04 00:44:48 -0600

    Insertion of tables into mySQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,3 +5,13 @@ INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGro
 INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGroup, summaryGroup,updateAt) VALUES('Cohort 16','2019-05-22','CuidadCreativa',30,true,'Grupo para convertirse en desarollador fullstack','2019-07-11');
 INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGroup, summaryGroup,updateAt) VALUES('Cohort 17','2019-07-02','GrupoChido',30,true,'Grupo para convertirse en desarollador fullstack','2019-10-25');
 INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGroup, summaryGroup,updateAt) VALUES('Cohort 18','2019-09-30','PapaMacho',30,true,'Grupo para convertirse en desarollador fullstack','2019-11-09');
+
+INSERT INTO course(nameCourse,summaryCourse,startDateCourse,endDateCourse,isEnableCourse,createAt) VALUES ('JavaFullstack','Desarrollo de conociemiento en tecnologias web mediante JAVA','2019-09-30','2019-12-30',true,'2019-08-30');
+INSERT INTO course(nameCourse,summaryCourse,startDateCourse,endDateCourse,isEnableCourse,createAt) VALUES ('English','Mejorar en el habla del idioma Ingles en un ambiente laboral','2019-09-30','2019-12-30',true,'2019-08-30');
+INSERT INTO course(nameCourse,summaryCourse,startDateCourse,endDateCourse,isEnableCourse) VALUES ('Mentoria','Mejorar las softskills del alumno','2019-09-30','2019-12-30',true);
+
+INSERT INTO activity(titleActivity) VALUES ('Series de Fiboncci');
+INSERT INTO activity(titleActivity) VALUES ('Email solution of problems');
+INSERT INTO activity(titleActivity) VALUES ('Preguntas para entrevista');
+
+INSERT INTO document(titleDocument,pathDoucument,isCheack) VALUES ('HOLA','path/path/path',true);
