54a4297d8cf1575245922075d1060410711b6cbd
Ale Durán <aduranrocha@gmail.com>
2020-01-04 00:21:13 -0600

    Interface of Activity Service

---
+++ src/main/java/com/gmoodle/models/services/IActivityService.java
@@ -0,0 +1,18 @@
+package com.gmoodle.models.services;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.gmoodle.models.entity.Activity;
+
+
+public interface IActivityService {
+	public List<Activity> findAll();
+	// Page the group list
+	public Page<Activity> findAll(Pageable pageable);
+	public Activity findById(Long id);
+	public Activity save(Activity activity);
+	public void delete(Long id);
+}
