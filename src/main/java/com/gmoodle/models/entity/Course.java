ce21f7ff27aeab13c349e5d54be216f615eeccb0
Ale Durán <aduranrocha@gmail.com>
2020-01-05 18:56:38 -0600

    Adding OneToMany in the table Course conexion with the table Activity

--- src/main/java/com/gmoodle/models/entity/Course.java
+++ src/main/java/com/gmoodle/models/entity/Course.java
@@ -1,18 +1,26 @@
 package com.gmoodle.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
+@Table(name="course")
 public class Course implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -32,4 +40,138 @@ public class Course implements Serializable {
 	@Column(name="createAt")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
+	
+	@NotNull(message ="can not be empty")
+	@Temporal(TemporalType.DATE)
+	private Date startDateCourse;
+	
+	@NotNull(message ="can not be empty")
+	@Temporal(TemporalType.DATE)
+	private Date endDateCourse;
+	
+	@NotNull(message="can not be empty")
+	@Column(nullable=false)
+	private boolean isEnableCourse;
+
+	@Column(nullable=true)
+	@Temporal(TemporalType.DATE)
+	private Date updateAt;
+	
+	//column that will connect with the other table
+	@OneToMany(mappedBy="course", cascade = CascadeType.ALL)
+	private List<Activity> activity = new ArrayList<>();
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @return the idCourse
+	 */
+	public Long getIdCourse() {
+		return idCourse;
+	}
+
+	/**
+	 * @param idCourse the idCourse to set
+	 */
+	public void setIdCourse(Long idCourse) {
+		this.idCourse = idCourse;
+	}
+
+	/**
+	 * @return the nameCourse
+	 */
+	public String getNameCourse() {
+		return nameCourse;
+	}
+
+	/**
+	 * @param nameCourse the nameCourse to set
+	 */
+	public void setNameCourse(String nameCourse) {
+		this.nameCourse = nameCourse;
+	}
+
+	/**
+	 * @return the summaryCourse
+	 */
+	public String getSummaryCourse() {
+		return summaryCourse;
+	}
+
+	/**
+	 * @param summaryCourse the summaryCourse to set
+	 */
+	public void setSummaryCourse(String summaryCourse) {
+		this.summaryCourse = summaryCourse;
+	}
+
+	/**
+	 * @return the createAt
+	 */
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	/**
+	 * @param createAt the createAt to set
+	 */
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	/**
+	 * @return the startDateCourse
+	 */
+	public Date getStartDateCourse() {
+		return startDateCourse;
+	}
+
+	/**
+	 * @param startDateCourse the startDateCourse to set
+	 */
+	public void setStartDateCourse(Date startDateCourse) {
+		this.startDateCourse = startDateCourse;
+	}
+
+	/**
+	 * @return the endDateCourse
+	 */
+	public Date getEndDateCourse() {
+		return endDateCourse;
+	}
+
+	/**
+	 * @param endDateCourse the endDateCourse to set
+	 */
+	public void setEndDateCourse(Date endDateCourse) {
+		this.endDateCourse = endDateCourse;
+	}
+
+	/**
+	 * @return the isEnableCourse
+	 */
+	public boolean getIsEnableCourse() {
+		return isEnableCourse;
+	}
+
+	/**
+	 * @param isEnableCourse the isEnableCourse to set
+	 */
+	public void setIsEnableCourse(boolean isEnableCourse) {
+		this.isEnableCourse = isEnableCourse;
+	}
+
+	/**
+	 * @return the updateAt
+	 */
+	public Date getUpdateAt() {
+		return updateAt;
+	}
+
+	/**
+	 * @param updateAt the updateAt to set
+	 */
+	public void setUpdateAt(Date updateAt) {
+		this.updateAt = updateAt;
+	}
 }
