1b775ffd796c8f10926cc5c352bb590783e88b22
Ale Durán <aduranrocha@gmail.com>
2020-01-06 19:11:13 -0600

    Add user, roles and user_roles before the other tables lines (1-7), because it was a problem in tables creation order

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,12 @@
+INSERT INTO `users` (username, name, lastName, email, password, isEnabled, gender, address, degree, phoneNumber, birthDate, photo, createAt,isDemoUser) VALUES ('JohnWick', 'John', 'Wick', 'mydog@email.com', '$2a$10$Y.aOM5A6u6wIwUXuAOLFzOTiQGmAud8s/8ZGm6FZSZMjvKYHnhEcu', true, false, 'Guadalajara Jalisco','Industrial Designer', '+52 3333121261', '1990-01-01', 'image', '2020-01-01',true);
+
+INSERT INTO `roles` (name) VALUES ('ROLE_ADMIN');
+INSERT INTO `roles` (name) VALUES ('ROLE_TEACHER');
+INSERT INTO `roles` (name) VALUES ('ROLE_STUDENT');
+
+INSERT INTO `users_roles` (user_id, role_id) VALUES (1, 1);
+
+
 INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGroup, summaryGroup,updateAt) VALUES('Cohort 12','2018-09-14','PapaMacho',30,false,'Grupo para convertirse en desarollador fullstack','2018-10-30');
 INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGroup, summaryGroup,updateAt) VALUES('Cohort 13','2018-11-27','LeoRocks',30,false,'Grupo para convertirse en desarollador fullstack','2018-12-15');
 INSERT INTO groupclass (nameGroup, createAt,enrolmentKey,idNumberMax,isEnableGroup, summaryGroup,updateAt) VALUES('Cohort 14','2019-01-30','Ivone',30,false,'Grupo para convertirse en desarollador fullstack','2019-03-30');
@@ -10,8 +19,8 @@ INSERT INTO course(nameCourse,summaryCourse,startDateCourse,endDateCourse,isEnab
 INSERT INTO course(nameCourse,summaryCourse,startDateCourse,endDateCourse,isEnableCourse,createAt,createById) VALUES ('English','Mejorar en el habla del idioma Ingles en un ambiente laboral','2019-09-30','2019-12-30',true,'2019-08-30',1);
 INSERT INTO course(nameCourse,summaryCourse,startDateCourse,endDateCourse,isEnableCourse,createAt,createById) VALUES ('Mentoria','Mejorar las softskills del alumno','2019-09-30','2019-12-30',true,'2019-08-30',1);
 
-INSERT INTO activity(titleActivity,instructions) VALUES ('Series de Fiboncci','Create a code that generates the Fibonnaci series util de the number the user ask');
-INSERT INTO activity(titleActivity,instructions) VALUES ('Email solution of problems','Write an email to solve a conflict');
-INSERT INTO activity(titleActivity,instructions) VALUES ('Interview questions','Make a list of interview questions');
+INSERT INTO activity(titleActivity,instructions,idCourse,idUser) VALUES ('Series de Fiboncci','Create a code that generates the Fibonnaci series util de the number the user ask',1,1);
+INSERT INTO activity(titleActivity,instructions,idCourse,idUser) VALUES ('Email solution of problems','Write an email to solve a conflict',1,1);
+INSERT INTO activity(titleActivity,instructions,idCourse,idUser) VALUES ('Interview questions','Make a list of interview questions',1,1);
 
 INSERT INTO document(titleDocument,pathDoucument,isCheack,isEnableDocument) VALUES ('HOLA','path/path/path',true,true);
